e3ea1345c1fc78acaa0d21e617afb807360bca42
maufierro1234 <mauferro1234@gmail.com>
2026-05-04 14:36:48 -0300

    Agregue el form y su css

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Formulario } from './Formulario';
+
+export default function AboutPage() {
+  return (
+    <main>
+      <header>
+      <h1>Aca vamos a empezar</h1>
+      <p>Primer parrafo del mundo</p>
+      </header>
+      <Formulario></Formulario>
+    </main>
+  )
+}
