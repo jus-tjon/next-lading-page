e2bb0514e1dda03c60d7aa783a44003e5f7bb342
Jon <jhoncueva194@gmail.com>
2026-05-06 19:56:25 -0300

    Cambié por completo el Hero y borré las imágenes q estaban por defecto

---
+++ app/components/ui/ButtonIcon.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+type ButtonIconProps = {
+  children: React.ReactNode;
+  Icon: React.ComponentType<{ className?: string }>;
+  href?: string;
+};
+
+const ButtonIcon = ({ children, Icon, href }: ButtonIconProps) => {
+  return (
+    <a href={href}>
+      <button
+        className={`btn justify-between btn-circle text-[16px] px-4 py-6 btn-neutral group flex hover:bg-white hover:text-black w-fit ${inter.className}`}
+      >
+        {children}
+        <span className="bg-white rounded-full ml-auto p-0.5 flex items-center justify-center group-hover:bg-black">
+          <Icon className="text-black w-5 h-5 group-hover:text-white " />
+        </span>
+      </button>
+    </a>
+  );
+};
+
+export default ButtonIcon;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Hero from "./components/sections/Hero";
+import Hero2 from "./components/sections/Hero2";
+import ButtonIcon from "./components/ui/ButtonIcon";
 import { Formulario } from "./components/ui/Formulario";
+import { ArrowDown } from "lucide-react";
 
 const Home = () => {
   return (
     <div>
       <main>
-        <Hero></Hero>
+        <Hero2 />
         <Formulario></Formulario>
       </main>
       Home
